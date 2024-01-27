b00f6bf42ce958b479aec9ad3f2a1567646d9c04
Khaiam <khaiam@mail.ru>
2024-01-27 09:03:40 +0300

    fix

--- error.vue
+++ error.vue
@@ -1,6 +1,12 @@
 <template>
-  <navbar :IsAuthenticated="IsAuthenticated" v-if="!pageLoading" @auth="$emit('auth')" @openBuyPopup="openBuyPopup"
-          :UserDetails="UserDetails" ref="navbar"></navbar>
+  <navbar
+      :IsAuthenticated="IsAuthenticated"
+      v-if="!pageLoading"
+      @auth="$emit('auth')"
+      @openBuyPopup="openBuyPopup"
+      :UserDetails="UserDetails"
+      ref="navbar"
+  ></navbar>
   <main>
     <section class="not-found">
       <div class="container not-found__inner">
@@ -15,14 +21,9 @@
             <span>Упс,</span> что-то пошло не так
           </h1>
           <p class="not-found__subtitle">
-            Пожалуйста перезагрузите страницу или вернитесь на
-            главную
+            Пожалуйста перезагрузите страницу или вернитесь на главную
           </p>
-          <div
-
-              class="button not-found__button"
-              @click="handleError"
-          >
+          <div class="button not-found__button" @click="handleError">
             На главную
           </div>
         </div>
@@ -30,62 +31,71 @@
     </section>
   </main>
   <buy-popup v-model:buy-popup-opened="buyPopupOpened"></buy-popup>
-  <Footer style="position: fixed; bottom: 0; right: 0; left: 0;" ></Footer>
-
+  <Footer style="position: fixed; bottom: 0; right: 0; left: 0"></Footer>
 </template>
 
 <script>
-
 import Footer from "@/components/Footer.vue";
 import Navbar from "@/components/Navbar.vue";
 import BuyPopup from "@/components/BuyPopup.vue";
 
 export default {
-  components: {BuyPopup, Navbar, Footer},
+  components: { BuyPopup, Navbar, Footer },
   data() {
     return {
       buyPopupOpened: false,
       IsAuthenticated: false,
       UserDetails: {},
-      pageLoading: false
-    }
+      pageLoading: false,
+    };
   },
   methods: {
     openBuyPopup() {
-      this.buyPopupOpened = true
+      this.buyPopupOpened = true;
     },
     async getUserDetails() {
-      const token = localStorage.getItem("token")
+      const token = localStorage.getItem("token");
       if (!token) {
-        this.IsAuthenticated = false
+        this.IsAuthenticated = false;
       } else {
-        this.pageLoading = true
-        const response = await fetch(this.$config.public.API_BASE_URL + "auth/user/", {
-          headers: {
-            "Authorization": "Token " + token
-          },
-          method: "GET",
-        })
-        console.log(response)
+        this.pageLoading = true;
+        const response = await fetch(
+            this.$config.public.API_BASE_URL + "auth/user/",
+            {
+              headers: {
+                Authorization: "Token " + token,
+              },
+              method: "GET",
+            }
+        );
+        console.log(response);
         if (response.status === 200) {
-          this.UserDetails = await response.json()
-          this.IsAuthenticated = true
+          this.UserDetails = await response.json();
+          this.IsAuthenticated = true;
         } else {
-          this.IsAuthenticated = false
-          localStorage.removeItem("token")
+          this.IsAuthenticated = false;
+          localStorage.removeItem("token");
         }
-        this.pageLoading = false
+        this.pageLoading = false;
       }
-    }
+    },
   },
-}
+};
 </script>
 
 <script setup>
-import "./assets/styles/style.css"
-const handleError = () => clearError({ redirect: "/" });
+import "@/assets/styles/style.css";
+
+useHead({
+  bodyAttrs: {
+    class: "error_page",
+  },
+});
+const handleError = () => clearError({redirect: "/"});
 </script>
 
-<style scoped>
-
+<style>
+.error_page .main-preloader {
+  display: none !important;
+}
 </style>
